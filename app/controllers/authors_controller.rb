895dba56e137b32cd8740c4d834a24c5934cb938
Yannis Hofmann <yannis.hofmann@student.hpi.uni-potsdam.de>
2022-11-07 15:51:47 +0100

    added name function and view of author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,6 +2,17 @@ class AuthorsController < ApplicationController
     def new
     end
     def create
-      render plain: params[:author].inspect
+      @author = Author.new(author_params)
+      @author.save
+      redirect_to @author
     end
+
+    def show
+      @author = Author.find(params[:id])
+    end
+
+    private
+      def author_params
+        params.require(:author).permit(:first_name, :last_name, :homepage)
+      end
 end
