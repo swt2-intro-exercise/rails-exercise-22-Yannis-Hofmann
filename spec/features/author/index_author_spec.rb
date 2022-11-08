393c09502a09535a0e52c9583901c764d397dfad
Yannis Hofmann <yannis.hofmann@student.hpi.uni-potsdam.de>
2022-11-08 10:30:22 +0100

    added test for new button in show all authors

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -7,4 +7,8 @@ describe "Author list page", :type => :feature do
     expect(page).to have_text("Alan Turing")
     expect(page).to have_link "Show", href: "/authors/1"
   end
+  it "should contain a link to create new authors" do
+    visit "/authors"
+    expect(page).to have_link 'New', href: new_author_path
+  end
 end
