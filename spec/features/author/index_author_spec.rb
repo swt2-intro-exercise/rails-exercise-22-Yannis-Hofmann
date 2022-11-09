d77ed1005d707f10668d8d7603fc3d55d6961b65
Yannis Hofmann <yannis.hofmann@student.hpi.uni-potsdam.de>
2022-11-09 14:45:39 +0100

    add test for Delete button and delete button

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -11,4 +11,9 @@ describe "Author list page", :type => :feature do
     visit "/authors"
     expect(page).to have_link 'New', href: new_author_path
   end
+  it "should contain a link to delete authors" do
+    @alan = FactoryBot.create(:author)
+    visit "/authors"
+    expect(page).to have_link 'Delete', href: author_path(@alan)
+  end
 end
