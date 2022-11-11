e14256d47e0727cc7bd01a0cc2465cd1b42f3a45
Yannis Hofmann <yannis.hofmann@student.hpi.uni-potsdam.de>
2022-11-11 15:26:13 +0100

    this time with all changes

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,5 +3,5 @@ class Paper < ApplicationRecord
   validates :venue, presence: true
   validates :year, presence: true, numericality: { only_integer: true }
   has_and_belongs_to_many :authors
-  scope :written_in, ->(filter_year) { where("year = ?", filter_year) }
+  scope :written_in, ->(filter_year) { where("year = ?", filter_year) if filter_year.present?}
 end
--- spec/features/paper/index_paper_spec.rb
+++ spec/features/paper/index_paper_spec.rb
@@ -15,5 +15,10 @@ describe "New paper page", type: :feature do
     visit "/papers"
     expect(page).to have_link("Delete", href: paper_path(@paper))
   end
+  it "should not not show a paper from 1950 when ?year=1951 is set" do
+    @paper = FactoryBot.create :paper
+    visit "/papers?year=1951"
+    expect(page).to_not have_text("COMPUTING MACHINERY AND INTELLIGENCE")
+  end
 end
 
